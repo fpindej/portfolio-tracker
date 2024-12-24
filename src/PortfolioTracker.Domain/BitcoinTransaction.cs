6fadb9ef179f2aac5a9fff2ae19a7751aaf17e8b
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-12-24 15:14:06 +0100

    Create domain project

---
+++ src/PortfolioTracker.Domain/BitcoinTransaction.cs
@@ -0,0 +1,18 @@
+﻿namespace PortfolioTracker.Domain;
+
+public class BitcoinTransaction
+{
+    public Guid Id { get; set; }
+
+    public DateTime TransactionDateTime { get; set; }
+
+    public decimal? BtcAmount { get; set; }
+
+    public string? BtcCurrency { get; set; }
+
+    public decimal? FiatPrice { get; set; }
+
+    public string? FiatCurrency { get; set; }
+
+    public string? Notes { get; set; }
+}
