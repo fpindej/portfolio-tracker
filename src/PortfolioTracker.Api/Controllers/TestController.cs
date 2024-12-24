6c7f2d2218358e25fd63e1708b05990ebbc31ee5
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-12-24 15:15:07 +0100

    WIP: implement csv testing endpoint

--- src/PortfolioTracker.Api/Controllers/TestController.cs
+++ src/PortfolioTracker.Api/Controllers/TestController.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioTracker.CsvParser.Models;
+using PortfolioTracker.CsvParser.Services;
+using PortfolioTracker.Domain;
 
 namespace PortfolioTracker.Api.Controllers;
 
@@ -9,15 +13,71 @@ namespace PortfolioTracker.Api.Controllers;
 [ApiExplorerSettings(GroupName = "v1")]
 public class TestController : ControllerBase
 {
+    private readonly CsvParsingService _csvParsingService;
+
+    public TestController(CsvParsingService csvParsingService)
+    {
+        _csvParsingService = csvParsingService ?? throw new ArgumentNullException(nameof(csvParsingService));
+    }
+
     [HttpGet]
     public IActionResult Get()
     {
         return Ok();
     }
 
-    [HttpPost]
-    public IActionResult Post()
+    [HttpPost("upload")]
+    [Consumes("multipart/form-data")]
+    public async Task<IActionResult> UploadCsv([FromForm] UploadCsvFormModel? formModel)
     {
-        throw new NotImplementedException();
+        if (formModel?.File.Length is 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
+
+        var file = formModel!.File;
+
+        try
+        {
+            await using var stream = file.OpenReadStream();
+
+            var dtos = _csvParsingService.FromCsv<BitcoinTransactionCsv>(stream);
+
+            var transactions = dtos.Select(ToBitcoinTransaction).ToList();
+
+            return Ok(transactions);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Failed to process the CSV file: {ex.Message}");
+        }
     }
+
+    private static BitcoinTransaction ToBitcoinTransaction(BitcoinTransactionCsv dto)
+    {
+        return new BitcoinTransaction
+        {
+            Id = dto.TransactionId,
+            TransactionDateTime = CombineDateAndTime(dto.Date, dto.Time),
+            BtcAmount = dto.BtcAmount,
+            BtcCurrency = dto.BtcCurrency,
+            FiatPrice = dto.FiatPrice,
+            FiatFee = dto.FiatFee,
+            FiatCurrency = dto.FiatCurrency,
+            Notes = dto.Notes
+        };
+    }
+    
+    private static DateTime CombineDateAndTime(DateTime date, TimeSpan time)
+    {
+        var combinedDateTime = date.Date + time;
+
+        return DateTime.SpecifyKind(combinedDateTime, DateTimeKind.Utc);
+    }
+}
+
+public class UploadCsvFormModel
+{
+    [Required]
+    public IFormFile File { get; set; } = null!;
 }
